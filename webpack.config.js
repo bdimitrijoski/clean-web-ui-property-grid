e20d8d9f7c4ca8042f1e18e1be24d4abae9f66a1
Branko Dimitrijoski <Branko.Dimitrijoski@endava.com>
2020-08-18 16:30:32 +0200

    webpack version upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
-var path = require("path");
-var webpack = require("webpack");
+var path = require('path');
+
+var webpack = require('webpack');
 
 var PATHS = {
   entryPoint: path.resolve(__dirname, 'src/index.ts'),
   bundles: path.resolve(__dirname, '_bundles'),
-}
+};
 
 var config = {
   // These are the entry point of our library. We tell webpack to use
@@ -13,7 +14,7 @@ var config = {
   // minification via UglifyJS
   entry: {
     'clean-web-ui-property-grid': [PATHS.entryPoint],
-    'clean-web-ui-property-grid.min': [PATHS.entryPoint]
+    'clean-web-ui-property-grid.min': [PATHS.entryPoint],
   },
   // The output defines how and where we want the bundles. The special
   // value `[name]` in `filename` tell Webpack to use the name we defined above.
@@ -24,12 +25,12 @@ var config = {
     filename: '[name].js',
     libraryTarget: 'umd',
     library: 'CleanWebUIPropertyGrid',
-    umdNamedDefine: true
+    umdNamedDefine: true,
   },
   // Add resolve for `tsx` and `ts` files, otherwise Webpack would
   // only look for common JavaScript file extension (.js)
   resolve: {
-    extensions: ['.ts', '.js']
+    extensions: ['.ts', '.js'],
   },
   // Activate source maps for the bundles in order to preserve the original
   // source when the user debugs the application
@@ -40,33 +41,35 @@ var config = {
     // using a RegEx on the name, which must end with `.min.js`
     // NB: Remember to activate sourceMaps in UglifyJsPlugin
     // since they are disabled by default!
-    /*new webpack.optimize.UglifyJsPlugin({
+    new webpack.optimize.UglifyJsPlugin({
       minimize: true,
       sourceMap: true,
       include: /\.min\.js$/,
-    })*/
+    }),
   ],
   module: {
     // Webpack doesn't understand TypeScript files and a loader is needed.
     // `node_modules` folder is excluded in order to prevent problems with
     // the library dependencies, as well as `__tests__` folders that
     // contain the tests for the library
-    loaders: [{
-      test: /\.ts?$/,
-      loader: 'awesome-typescript-loader',
-      exclude: /node_modules/,
-      query: {
-        // we don't want any declaration file in the bundles
-        // folder since it wouldn't be of any use ans the source
-        // map already include everything for debugging
-        declaration: false,
-      }
-    }]
+    loaders: [
+      {
+        test: /\.ts?$/,
+        loader: 'awesome-typescript-loader',
+        exclude: /node_modules/,
+        query: {
+          // we don't want any declaration file in the bundles
+          // folder since it wouldn't be of any use ans the source
+          // map already include everything for debugging
+          declaration: false,
+        },
+      },
+    ],
   },
   stats: {
     colors: {
       green: '\u001b[32m',
-    }
+    },
   },
 
   devServer: {
@@ -88,9 +91,9 @@ var config = {
       warnings: true,
       colors: {
         green: '\u001b[32m',
-      }
+      },
     },
-  }
-}
+  },
+};
 
 module.exports = config;
